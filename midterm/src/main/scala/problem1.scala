9874ad915447d8e6e5ffa4d3ee07a387e9a0d93a
Addison Kauzer <ak94510@gmail.com>
2018-03-21 10:47:55 -0700

    first

--- midterm/src/main/scala/problem1.scala
+++ midterm/src/main/scala/problem1.scala
@@ -1,5 +1,7 @@
 object problem1 extends App {
-  def pairs(lst: List[Int]) : List[(Int, Int)] = ...
+  def pairs(lst: List[Int]) : List[(Int, Int)] = {
+    lst.grouped(2).Map(_).toList
+  }
   
   println(pairs(List(1, 2, 3, 4, 5, 6)))
   println(pairs(List(1, 2, 3, 4, 5)))
