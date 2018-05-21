8003e6ca4251a5bf778d5841c1e03bb16bd79f0a
Addison Kauzer <ak94510@gmail.com>
2018-05-21 10:27:41 -0700

    question1

---
+++ final/problem1.bak
@@ -0,0 +1,3 @@
+(define (shift lst elem) ...)
+(define (normalizeCycle cycle) (shift cycle (first (sort cycle <))))
+(define (normalize cycles) ...)
--- final/problem1.rkt
+++ final/problem1.rkt
@@ -1,3 +1,4 @@
-(define (shift lst elem) ...)
+(define (shiftonce cycle) (cons (last cycle) (remove (last cycle) cycle)))
+(define (shift cycle element) (if (= (first cycle) element) cycle (shift (shiftonce cycle) element)))
 (define (normalizeCycle cycle) (shift cycle (first (sort cycle <))))
-(define (normalize cycles) ...)
+(define (normalize list) (sort (map (lambda (cycle) (shift cycle (first (sort cycle <)))) list) (lambda (x y) (< (first x) (first y)))))
