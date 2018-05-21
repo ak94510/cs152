a440c9a070756d2df4fa53d6c08fecf301f890fc
Addison Kauzer <ak94510@gmail.com>
2018-05-21 11:57:53 -0700

    Quick fixes, final commit

--- final/problem4/src/main/scala/MultiMap.scala
+++ final/problem4/src/main/scala/MultiMap.scala
@@ -1,9 +1,9 @@
 // Fix variance as needed
 class MultiMap[-K,+V](entries: List[(K,V)] = List()) {
   // Implement get
-  def get[T <: K](k: T): Option[V] = entries.find(p => p._1 == k) match {
-    case Some(p) => Some(p._2)
-    case None => None
+  def get[T <: K](k: T): List[V] = entries.find(p => p._1 == k) match {
+    case Some(p) => p._2 :: Nil
+    case None => List()
    }
 
   // Fix put as needed
